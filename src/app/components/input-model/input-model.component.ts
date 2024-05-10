0f0767a344b2bbd11aec98b651b971ca7b7afd14
LuizSampaio-cpu <luizhorta2910@gmail.com>
2024-05-10 15:03:21 -0300

    feat: login page layout

--- src/app/components/input-model/input-model.component.ts
+++ src/app/components/input-model/input-model.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, EventEmitter, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 
 type InputTypes = "text" | "email" | "password"
 
@@ -9,13 +9,47 @@ type InputTypes = "text" | "email" | "password"
   imports: [
     ReactiveFormsModule
   ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputModelComponent),
+      multi: true
+    }
+  ],
   templateUrl: './input-model.component.html',
   styleUrl: './input-model.component.scss'
 })
-export class InputModelComponent {
+export class InputModelComponent implements ControlValueAccessor {
+
+
+
   @Input() type: InputTypes = "text";
   @Input() formName: string = "";
   @Input() placeholder: string = "";
   @Input() label: string = "";
+
+  value: string = '';
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  onInput(event: Event) {
+    const value = (event.target as HTMLInputElement).value
+    this.onChange(value)
+
+  }
+
+  writeValue(value: any): void {
+    this.value = value
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {}
+
+  registerOnTouched(fn: any): void {
+     this.onTouched = fn;
+  }
 
 }
