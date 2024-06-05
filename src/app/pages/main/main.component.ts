b7acc920b82517e296823841ab19dbc3373a4fd2
LuizSampaio-cpu <luizhorta2910@gmail.com>
2024-06-05 13:27:50 -0300

    add menu

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { PaginatorModule } from 'primeng/paginator';
 import { TableModule } from 'primeng/table'
+import { MenuModule } from 'primeng/menu'
+import { CommonModule } from '@angular/common';
+import { PrimeIcons } from 'primeng/api';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [PaginatorModule, TableModule],
+  imports: [CommonModule, PaginatorModule, TableModule, ButtonModule, MenuModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
@@ -25,6 +29,12 @@ export class MainComponent {
       { codigo: '010', abertoHa: '12 dias', situacao: 'Pendente', ultimaAcao: 'Aguardando cliente' },
       { codigo: '011', abertoHa: '9 dias', situacao: 'Concluído', ultimaAcao: 'Problema resolvido' },
       { codigo: '012', abertoHa: '3 dias', situacao: 'Em andamento', ultimaAcao: 'Teste em progresso' },
+  ]
+
+  items = [
+    {label: 'Novo Chamado', route: '/new', url: 'https://localhost:4200/new', icon: PrimeIcons.PLUS},
+    {label: 'Meus Chamados', route: '/main', url: 'https://localhost:4200/main', icon: PrimeIcons.HOME},
+    {label: 'Acompanhamentos', route: '/acompanhamentos', url: 'https://localhost:4200/acompanhamentos', icon: PrimeIcons.SEARCH},
   ]
 
 
